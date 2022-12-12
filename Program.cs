ade1ccdffa6b2a3693a945c41e836f8b539f2220
lcsmota <sacul_m@protonmail.com>
2022-12-12 09:10:14 -0300

    Add swagger with JWT support

--- Program.cs
+++ Program.cs
@@ -1,13 +1,41 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(sw =>
+    {
+        sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer[espaço] e o seu token.\r\n\r\nExemplo: Bearer AJufwlsadflkASOLKMYET...\r\n\r\n",
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey
+        });
+
+        sw.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+
+                new string[] {}
+            }
+        });
+    });
 
     builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
